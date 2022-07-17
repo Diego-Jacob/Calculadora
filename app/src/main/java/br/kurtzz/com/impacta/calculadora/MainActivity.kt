5f04e68b16b4850bc29ef2f0432ff1302a75aa96
Diego Jacob <djnovaes@gmail.com>
2022-07-16 23:26:15 -0300

    Start project

---
+++ app/src/main/java/br/kurtzz/com/impacta/calculadora/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.kurtzz.com.impacta.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
