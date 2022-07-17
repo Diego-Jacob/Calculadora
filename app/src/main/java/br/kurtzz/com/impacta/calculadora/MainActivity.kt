74da59b0e6fc8609b2aba1f27d92405ca6e21e5d
Diego Jacob <djnovaes@gmail.com>
2022-07-17 20:13:55 -0300

    Layout Completo Botões 0,1,2 e Apagar Funções digitaNumero, apagaNumero e apagaTudo

--- app/src/main/java/br/kurtzz/com/impacta/calculadora/MainActivity.kt
+++ app/src/main/java/br/kurtzz/com/impacta/calculadora/MainActivity.kt
@@ -7,15 +7,56 @@ import br.kurtzz.com.impacta.calculadora.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity()
 {
 
-    var _binding: ActivityMainBinding? = null
-    val binding get() = _binding!!
+       var _binding: ActivityMainBinding? = null
+       val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
-        super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        var root = binding.root
-        setContentView(root)
+       super.onCreate(savedInstanceState)
+       _binding = ActivityMainBinding.inflate(layoutInflater)
+       var root = binding.root
+       setContentView(root)
+       var guardaString: String = ""
+
+       fun digitaNumero (v1: String)
+       {
+          guardaString += v1
+          binding.Visor1.text = "$guardaString"
+       }
+
+        fun apagaNumero ()
+        {
+           guardaString = guardaString.dropLast(1)
+           binding.Visor1.text = "${guardaString}"
+        }
+
+        fun apagaTudo()
+        {
+           guardaString = ""
+           binding.Visor1.text = guardaString
+           binding.Visor2.text = "Apagou Tudo"
+        }
+
+        binding.Zero.setOnClickListener{
+            if (guardaString != "") digitaNumero("0") }
+
+        binding.Um.setOnClickListener{
+            digitaNumero("1")
+        }
+
+        binding.Dois.setOnClickListener{
+            digitaNumero("2") }
+
+        binding.Apagar.setOnClickListener {
+            apagaNumero()
+        }
+
+        binding.Apagar.setOnLongClickListener{
+            apagaTudo()
+            true
+        }
+
+
 
 
     }
